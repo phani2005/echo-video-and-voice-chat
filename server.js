a3b34f0508fa9cb12bedaf88bbb76e36bf438961
phani2005 <125913140+phani2005@users.noreply.github.com>
2026-03-18 11:22:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ import { createServer } from "http"
 import { Server } from "socket.io"
 import { type } from "os"
 import dotenv from "dotenv"
+import cors from "cors"
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"))
 const httpServer = createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
 const PORT=process.env.PORT||5000
 mongoose.connect(process.env.MONGO_URL)
     .then(() => { console.log("Mongodb is connected successfully") })
